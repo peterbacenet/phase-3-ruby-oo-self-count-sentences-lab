1a01ec6d98c1367a6f5cae92fa3d75af3baf9681
Peter Bacenet <peterbacenet@icloud.com>
2021-09-26 13:42:59 -0400

    commit tree 9ecb8ca230e4f8d28b565128aa612e30f0d36f73

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
-  def question?
 
+  def question?
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #can only split one without regex
+    #converts to array
+    three = self.split(/\.|\?|\!/)
+    number_of_sentences = three.length.to_int
   end
 end
+
+one = String.new("Hello.")
+two = String.new("Hello. I am Peter? What are you!")
